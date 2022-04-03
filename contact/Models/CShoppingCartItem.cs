dbafcf19b6daf8dcfb2b56b6dbc781e5e642eb93
mo mo <motomato914@gmail.com>
2022-04-03 20:45:04 +0800

    Models/CShoppingCartItempublic 增加 int 庫存數量 { get; set; }

--- contact/Models/CShoppingCartItem.cs
+++ contact/Models/CShoppingCartItem.cs
@@ -17,5 +17,7 @@ namespace contact.Models
         public decimal 單價 { get; set; }
         public decimal 小計 { get { return this.數量 * this.單價; } }
         public t產品 Product { get; set; }
+
+        public int 庫存數量 { get; set; }
     }
 }
